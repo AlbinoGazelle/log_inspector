c48528b10f13d97f5cf1dba4163219422055d8f3
burnsn3 <burnsn3@wwu.edu>
2021-01-31 13:36:41 -0800

    add checking for arguments and permission error checks

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 import subprocess
 import os
 import sys
-#this function checks if a program is installed or not
-def is_installed(name):
-    try:
-        subprocess.Popen([name], stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL).communicate()
-    except OSError:
-        return False
-    return True
+
+
+
+
 #Jan 30 19:56:58 ubuntu su: FAILED SU (to root) exp on pts/1
 def authlog_reader(name):
     try:
@@ -22,7 +19,12 @@ def authlog_reader(name):
             #Jan 31 00:49:07 tryhackme sudo:   albino : 2 incorrect password attempts ; TTY=pts/0 ; PWD=/home/albino/Desktop ; USER=root ; COMMAND=/usr/bin/apt-get install apache2
             elif "incorrect password attempts" in x:
                 print(f"user: {txt[7]} tried to execute: {x[x.find('COMMAND'):len(x) - 1]} at {txt[0], txt[1], txt[2]} as the {txt[18]}")
-
     except FileNotFoundError:
         print("Couldn't find file, please enter full path!")
-authlog_reader(sys.argv[1])
+    except PermissionError:
+        print("Insufficient permissions to read this file, maybe run as sudo?")
+if len(sys.argv) !=2:
+    print("usage: ./inspector.sh path_to_log_file")
+    exit()
+elif "auth.log" in sys.argv[1]:
+    authlog_reader(sys.argv[1])
