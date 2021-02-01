15530adeccc667d597ebc70e71a3f6ddc3d1fac2
burnsn3 <burnsn3@wwu.edu>
2021-01-31 23:00:40 -0800

    add additional checks and print how many times specific users did something

--- main.py
+++ main.py
@@ -1,28 +1,53 @@
-import subprocess
-import os
 import sys
-
-
-
-
+#helper function for eliminating 
+def get_unique(list):
+    unique = []
+    for x in list:
+        if x not in unique:
+            unique.append(x)
+    return unique
 #Jan 30 19:56:58 ubuntu su: FAILED SU (to root) exp on pts/1
 def authlog_reader(name):
+    password_attempt = []
+    failed_attempt = []
+    command_attempt = []
+    print("Printing useful information: \n")
     try:
         f = open(sys.argv[1], "r")
         for x in f:
             txt = x.split(" ")
             #get lines for failed SU attemps
             if "FAILED" in x:
+                failed_attempt.append(txt[9])
                 print(f"user: {txt[9]} attempted to SU to user: {txt[8].rstrip(')')} at {txt[0], txt[1], txt[2]}")
+            #get lines for commands that are executed
             elif "COMMAND" in x and "incorrect password" not in x:
+                command_attempt.append(txt[7])
                 print(f"user: {txt[7]} executed: {x[x.find('COMMAND'):len(x) - 1]} at {txt[0], txt[1], txt[2]}")
             #Jan 31 00:49:07 tryhackme sudo:   albino : 2 incorrect password attempts ; TTY=pts/0 ; PWD=/home/albino/Desktop ; USER=root ; COMMAND=/usr/bin/apt-get install apache2
+            #get lines for failed sudo executes
             elif "incorrect password attempts" in x:
+                password_attempt.append(txt[7])
                 print(f"user: {txt[7]} tried to execute: {x[x.find('COMMAND'):len(x) - 1]} at {txt[0], txt[1], txt[2]} as the {txt[18]}")
     except FileNotFoundError:
         print("Couldn't find file, please enter full path!")
     except PermissionError:
         print("Insufficient permissions to read this file, maybe run as sudo?")
+    print(" ")
+    print("Printing how many times each user did something:\n")
+    #print the number of times users did something
+    unique_users_password = get_unique(password_attempt)
+    for x in unique_users_password:
+        print(f"{x} attempted to execute a command {password_attempt.count(x)} times ")
+
+    unique_users_failed = get_unique(failed_attempt)
+    for x in unique_users_failed:
+        print(f"{x} attempted to SU to a user {failed_attempt.count(x)} times ")
+
+    unique_users_command = get_unique(command_attempt)
+    for x in unique_users_command:
+        print(f"{x} executed a command {command_attempt.count(x)} times ")
+
 if len(sys.argv) !=2:
     print("usage: ./inspector.sh path_to_log_file")
     exit()
