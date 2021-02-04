a935c25424c45aae60221947f59fc537d5ed7971
burnsn3 <burnsn3@wwu.edu>
2021-02-03 17:22:33 -0800

    add verbosity flag

--- main.py
+++ main.py
@@ -1,47 +1,59 @@
 import sys
-#helper function for eliminating unique values from a list
+
+print_check = False
+if("-v" in sys.argv):
+        print_check =  True
+if(print_check == False):
+    print("Use the -v flag to increase verbositity and display what each user did!\nExample: ./inspector -v path_to_log\n")
+
+#helper function for getting unique values from a list
 def get_unique(list):
     unique = []
     for x in list:
         if x not in unique:
             unique.append(x)
     return unique
-#Jan 30 19:56:58 ubuntu su: FAILED SU (to root) exp on pts/1
+#main function for reading /var/log/auth.log
 def authlog_reader(name):
     password_attempt = []
     failed_attempt = []
     command_attempt = []
     auth_failure = []
-    print("Printing useful information: \n")
     try:
-        f = open(sys.argv[1], "r")
+        f = open(name, "r")
         for x in f:
             txt = x.split()
             #get lines for failed SU attemps
             if "FAILED" in x:
                 failed_attempt.append(txt[9])
-                print(f"user: {txt[9]} attempted to SU to user: {txt[8].rstrip(')')} at {txt[0], txt[1], txt[2]}")
+                if(print_check): 
+                    print(f"user: {txt[9]} attempted to SU to user: {txt[8].rstrip(')')} at {txt[0], txt[1], txt[2]}")
             #get lines for commands that are executed
             elif "COMMAND" in x and "incorrect password" not in x:
                 #fix weird colon issue I was getting in 20.04
                 if ":" in x:
                     txt[5] = txt[5].replace(":", "")
                 command_attempt.append(txt[5])
-                print(f"user: {txt[5]} executed: {x[x.find('COMMAND'):len(x) - 1]} at {txt[0], txt[1], txt[2]}")
+                if(print_check):
+                    print(f"user: {txt[5]} executed: {x[x.find('COMMAND'):len(x) - 1]} at {txt[0], txt[1], txt[2]}")
             #get lines for failed password attempts
             elif "incorrect password attempts" in x:
                 password_attempt.append(txt[5])
-                print(f"user: {txt[5]} tried to execute: {x[x.find('COMMAND'):len(x) - 1]} at {txt[0], txt[1], txt[2]} as the {txt[16]}")
+                if(print_check):
+                    print(f"user: {txt[5]} tried to execute: {x[x.find('COMMAND'):len(x) - 1]} at {txt[0], txt[1], txt[2]} as the {txt[16]}")
             elif "authentication failure" in x:
                 auth_failure.append(txt[14])
-                print(f"{txt[14]} failed to authenticate as another user!")
-    except FileNotFoundError:
-        print("Couldn't find file, please enter full path!")
+                if(print_check):
+                    print(f"{txt[14]} failed to authenticate as another user!")
+    except FileNotFoundError as e:
+        print("Couldn't find file, please enter full path!" + e)
+        exit()
     except PermissionError:
         print("Insufficient permissions to read this file, maybe run as sudo?")
-    print(" ")
-    print("Printing how many times each user did something:\n")
+        exit()
+    print("Printing how many times each user did something:")
     #print the number of times users did something
+
     unique_users_password = get_unique(password_attempt)
     for x in unique_users_password:
         print(f"{x} attempted to execute a command {password_attempt.count(x)} times ")
@@ -56,9 +68,10 @@ def authlog_reader(name):
 
     unique_users_auth = get_unique(auth_failure)
     for x in unique_users_auth:
-        print(f"{x} tried authentication as a user {auth_failure.count(x)} times")
-if len(sys.argv) !=2:
+        print(f"{x} tried authenticating as a user {auth_failure.count(x)} times")
+
+if len(sys.argv) <2:
     print("usage: ./inspector.sh path_to_log_file")
     exit()
-elif "auth.log" in sys.argv[1]:
-    authlog_reader(sys.argv[1])
+elif "auth.log" in sys.argv[len(sys.argv) - 1]:
+    authlog_reader(sys.argv[len(sys.argv) - 1])
